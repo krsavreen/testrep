81bc116253c7ea562a6a78fd12ef276f609b20f3
Savvy <krsavreen@gmail.com>
2021-01-13 13:05:42 -0700

    First Commit.

---
+++ test.java
@@ -0,0 +1,17 @@
+public class test 
+{
+
+	public static void main(String[] args) 
+	{		
+		System.out.println("Hello World!");
+        
+		greet();
+        
+		System.out.println("End of main()");
+	}
+	
+	public static void greet()
+	{
+     System.out.println("Hi! How are you?");
+	}
+}
